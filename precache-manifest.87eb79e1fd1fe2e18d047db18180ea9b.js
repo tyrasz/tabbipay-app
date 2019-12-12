c0dd4433f0568edd0cba1d2e7eb8370d0b27bb14
tyrasz <wzhikai@gmail.com>
2019-12-13 01:43:35 +0800

    Updates

---
+++ precache-manifest.87eb79e1fd1fe2e18d047db18180ea9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ced6ae3f05a6832209698f8dfa347",
+    "url": "/tabbipay-app/index.html"
+  },
+  {
+    "revision": "2aec57b991e6e13a053a",
+    "url": "/tabbipay-app/static/css/main.92e8ffde.chunk.css"
+  },
+  {
+    "revision": "f30c36cab6fa20938bda",
+    "url": "/tabbipay-app/static/js/2.d0698231.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tabbipay-app/static/js/2.d0698231.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aec57b991e6e13a053a",
+    "url": "/tabbipay-app/static/js/main.f02790db.chunk.js"
+  },
+  {
+    "revision": "8c81b7477beb380ff8e9",
+    "url": "/tabbipay-app/static/js/runtime-main.ab39eac5.js"
+  },
+  {
+    "revision": "8f2fecdaf8c6737b238e2437c5e92e57",
+    "url": "/tabbipay-app/static/media/Logo.8f2fecda.png"
+  },
+  {
+    "revision": "71b22db5190a02e420ae4fb456732ca0",
+    "url": "/tabbipay-app/static/media/Man.71b22db5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tabbipay-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/Terms.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Terms() {
-  return (
-    <h1>About page </h1>
-  );
-}
-
-export default Terms;
